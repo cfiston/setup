0030f8131e6365b0547c1f5ab826a5b653bdb574
Fiston <cfiston@gmail.com>
2019-02-24 21:44:29 -0500

    Updates

--- ambari-bootstrap/extras/deploy/install-ambari-bootstrap.sh
+++ ambari-bootstrap/extras/deploy/install-ambari-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo yum -y -q install git
-sudo git clone https://cfiston:ba9ff9b377e737ce5a967c034aca0005591ec276@github.com/cfiston/keibacloud.git /opt/keibacloud
+sudo git clone https://b1cd16e57ca2b8e3b62459351d451d7e70d9734a@github.com/cfiston/keibacloud.git /opt/keibacloud
 sudo chmod 777 -R /opt/keibacloud
 cd chmod -R g+rw /opt/keibacloud/ambari-bootstrap
 sudo chown -R ${USER}:users /opt/keibacloud/ambari-bootstrap
